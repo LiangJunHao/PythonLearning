3259ae732e2c830a1c8ede9021678a842f23bc38
LiangJunHao <1342348969@qq.com>
2018-05-14 00:37:25 +0800

    Summary for Exercise6 and Exercise7

---
+++ Exercise22/ex6_7.py
@@ -0,0 +1,6 @@
+print("This is the {}.".format('test'))
+name = 'junhao {}'
+Hello_world = "Hello World!"
+print(name.format('Liang'))
+test_content = f"This is a demo, just see what happen! {Hello_world}"
+print(test_content)
